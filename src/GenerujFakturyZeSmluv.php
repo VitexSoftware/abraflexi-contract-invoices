60a45633d4d20e4ba9de91b97a9b6f21c22aa54a
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-10-02 00:38:22 +0200

    v1.0 release

--- src/GenerujFakturyZeSmluv.php
+++ src/GenerujFakturyZeSmluv.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace FlexiPeeHP;
+namespace AbraFlexi;
 
 use Ease\Shared;
 
@@ -18,56 +18,58 @@ if (file_exists('../client.json')) {
     $shared->loadConfig('../client.json', true);
 }
 
+$envFile = dirname(__DIR__) . '/.env';
+if (file_exists($envFile)) {
+    \Ease\Shared::singleton()->loadConfig($envFile, true);
+}
 
-$invoicer     = new FakturaVydana();
-$contractor   = new Smlouva(null,['debug'=>false]);
-$contractor->logBanner(constant('EASE_APPNAME'));
-$contractList = $contractor->getColumnsFromFlexibee(['id', 'kod', 'nazev', 'firma'],['autoGen'=>true]);
-foreach ($contractList as $counter => $contractInfo) {
-    $message = $counter.'/'.count($contractList).' '.$contractInfo['nazev'].' '.$contractInfo['firma@showAs'];
+$invoicer = new FakturaVydana();
+$contractor = new Smlouva();
+$contractor->logBanner(\Ease\Shared::appName());
+$contractList = $contractor->getColumnsFromAbraFlexi(['id', 'kod', 'nazev', 'firma'], ['autoGen' => true, 'limit' => 0]);
+if ($contractList) {
+    foreach ($contractList as $counter => $contractInfo) {
+        $message = $counter . '/' . count($contractList) . ' ' . $contractInfo['nazev'] . ' ' . $contractInfo['firma@showAs'];
 
-    $contractor->setMyKey($contractInfo['id']);
+        $contractor->setMyKey($contractInfo['id']);
 
-    if ($contractor->generovaniFaktur()) {
-        if (array_key_exists('messages', $contractor->lastResult)) {
-            if (strstr($contractor->lastResult['messages']['message'], 'Nebyla')) {
-                $contractor->addStatusMessage($message, 'debug');
-            } else {
+        if ($contractor->generovaniFaktur()) {
+            if (array_key_exists('messages', $contractor->lastResult)) {
+                if (strstr($contractor->lastResult['messages']['message'], 'Nebyla')) {
+                    $contractor->addStatusMessage($message, 'debug');
+                } else {
 
-                if (strstr($contractor->lastResult['messages']['message'],
-                        'faktur') && strstr($contractor->lastResult['messages']['message'],
-                        ':')) {
-                    $hmr       = explode(':',
-                        $contractor->lastResult['messages']['message']);
-                    $howMany   = intval(end($hmr));
-                    $generated = $invoicer->getColumnsFromFlexibee(['kod'],
-                        [
-                            'firma' => \FlexiPeeHP\FlexibeeRO::code($contractInfo['firma']),
-                            'cisSml' => $contractInfo['kod'],
-                            'limit' => $howMany
-                    ]);
+                    if (strstr($contractor->lastResult['messages']['message'],
+                                    'faktur') && strstr($contractor->lastResult['messages']['message'],
+                                    ':')) {
+                        $hmr = explode(':',
+                                $contractor->lastResult['messages']['message']);
+                        $howMany = intval(end($hmr));
+                        $generated = $invoicer->getColumnsFromAbraFlexi(['kod'],
+                                [
+                                    'firma' => \AbraFlexi\RO::code($contractInfo['firma'][0]),
+                                    'cisSml' => $contractInfo['kod'],
+                                    'limit' => $howMany
+                        ]);
 
-                    $invoices = [];
-                    foreach ($generated as $result) {
-                        $invoices[] = $result['kod'];
+                        $invoices = [];
+                        foreach ($generated as $result) {
+                            $invoices[] = $result['kod'];
+                        }
+
+                        $contractor->addStatusMessage($message . ' ' . implode(',',
+                                        $invoices), 'success');
                     }
-
-                    $contractor->addStatusMessage($message.' '.implode(',',
-                            $invoices), 'success');
+                }
+            } else {
+                if (array_key_exists('success', $contractor->lastResult) && ($contractor->lastResult['success'] == 'failed')) {
+                    $contractor->addStatusMessage($message . ' ' . $notice, 'warning');
                 }
             }
         } else {
-            if (array_key_exists('success', $contractor->lastResult) && ($contractor->lastResult['success']
-                == 'failed')) {
-                $contractor->addStatusMessage($message.' '.$notice, 'warning');
-            }
+            $status = $contractor->lastResponseCode == 500 ? 'error' : 'warning';
+            $notice = str_replace('"', ' ', trim(json_encode($contractor->getErrors()), '[{}]'));
+            $contractor->addStatusMessage($message . ' ' . $notice, $status);
         }
-    } else {
-        $status = $contractor->lastResponseCode == 500 ? 'error' : 'warning';
-        $notice = str_replace('"', ' ',
-            trim(json_encode($contractor->errors), '[{}]'));
-        $contractor->addStatusMessage($message.' '.$notice, $status);
     }
 }
-
-
